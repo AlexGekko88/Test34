70b1eb9ef94ed5b5a7e9e990378efed042f237e8
AlexGekko88 <115148133+AlexGekko88@users.noreply.github.com>
2022-12-19 16:18:56 +0300

    Initial commit

---
+++ src/Dog.java
@@ -0,0 +1,10 @@
+public class Dog extends Animal {
+    @Override
+    public void eat() {
+        System.out.println("Dog is !!!!!!!!!!");
+    }
+
+    public void bark() {
+        System.out.println("Dog is barking.....");
+    }
+}
