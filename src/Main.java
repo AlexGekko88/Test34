26a01c55add2696d6e59925a8e09d340c89465b3
AlexGekko88 <115148133+AlexGekko88@users.noreply.github.com>
2022-12-20 11:39:51 +0300

    Выполнил Правки

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Animal animal = new Dog();
         animal.eat();
-        System.out.println("Привет мир, Привет привет!!!");
+        System.out.println("Привет мир, Привет привет?????");
         Cat cat1 = new Cat();
    }
 }
